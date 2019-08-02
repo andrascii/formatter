b6dbd6c72df815b995afedb39943c431c3bf395c
Andrey <apugachevdev@gmail.com>
2019-08-03 01:18:29 +0300

    Added command to convert file to utf8

--- formatter.py
+++ formatter.py
@@ -2,6 +2,57 @@ import subprocess
 from argparse import ArgumentParser
 from os import listdir
 from os.path import isfile, join, splitext
+from abc import ABCMeta, abstractmethod
+from bs4 import UnicodeDammit
+
+
+def get_file_content(file):
+    with open(file, mode='rb') as stream:
+        return stream.read()
+
+
+def convert_file_to_utf8(file, from_encoding):
+    tmp_file = file + '-tmp'
+    execute_command('iconv --from-code={0} --to-code=UTF-8 "{1}" > "{2}"'.format(from_encoding, file, tmp_file))
+    execute_command('rm "{0}"'.format(file))
+    execute_command('mv "{0}" "{1}"'.format(tmp_file, file))
+
+
+class Worker:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def do(self, file: str):
+        raise NotImplementedError
+
+
+class PrependPchWorker(Worker):
+    def __init__(self, pch):
+        self.__pch = pch
+
+    def do(self, file: str):
+        self.__prepend_pch(file, self.__pch)
+        print('#include "stdafx.h" into', file)
+
+    def __prepend_pch(self, file, pch):
+        content = get_file_content(file)
+        content = '#include "{0}"\n'.format(pch).encode('ascii') + content
+        self.__write_content(file, content)
+
+    @staticmethod
+    def __write_content(file, content):
+        with open(file, mode='wb') as stream:
+            stream.write(content)
+
+
+class ToUtf8Worker(Worker):
+    def do(self, file: str):
+        content = get_file_content(file)
+        dammit = UnicodeDammit(content)
+
+        if dammit.original_encoding != 'utf-8':
+            print('Encode', file, 'to utf-8')
+            convert_file_to_utf8(file, dammit.original_encoding)
 
 
 def execute_command(command):
@@ -21,8 +72,7 @@ def get_all_files(path, extensions, to_exclude_dirs=[], to_exclude_files=[]):
 
             stripped_file_extension = file_extension.strip(".")
 
-            extension_available = not extensions and not file_extension or \
-                stripped_file_extension in extensions
+            extension_available = not extensions or stripped_file_extension in extensions
 
             if is_file and extension_available and f not in to_exclude_files:
                 all_files.append(full_path)
@@ -41,62 +91,20 @@ def get_all_files(path, extensions, to_exclude_dirs=[], to_exclude_files=[]):
 
 def get_params():
     parser = ArgumentParser()
+
+    # files selection settings
     parser.add_argument('--path', help='Path to the target directory')
     parser.add_argument('--extension', help='Target files extension')
     parser.add_argument('--dir_exclude', help='Directories to exclude from search')
     parser.add_argument('--file_exclude', help='File names to exclude')
+
+    # commands
     parser.add_argument('--prepend_pch', help='Insert include for passed header file first for all selected files')
+    parser.add_argument('--conv_to_utf8',
+                        help='All selected files not in UTF-8 encoding will be converted to UTF-8',
+                        action='store_true')
+
     return parser.parse_args()
-
-
-class PrependPchWorker:
-    def __init__(self, target_files, pch):
-        self.__target_files = target_files
-        self.__pch = pch
-
-    def do(self):
-        for file in self.__target_files:
-            self.__prepend_pch(file, self.__pch)
-            print('Wrote to', file)
-
-        print('Modified', len(self.__target_files), 'files')
-
-    def __get_file_content(self, file):
-        try:
-            with open(file) as stream:
-                return stream.read()
-
-        except UnicodeDecodeError:
-            charset = self.__get_file_charset(file)
-            self.__convert_file_to_utf8(file, charset)
-
-            with open(file) as stream:
-                return stream.read()
-
-    def __prepend_pch(self, file, pch):
-        content = self.__get_file_content(file)
-        content = '#include "{0}"\n'.format(pch) + content
-        self.__write_content(file, content)
-
-    @staticmethod
-    def __write_content(file, content):
-        with open(file, mode='w') as stream:
-            stream.write(content)
-
-    @staticmethod
-    def __get_file_charset(file):
-        result = execute_command('file -bi ' + file)
-        # parse string like this one: type/x-c++; charset=cp1251
-        _, charset = result.stdout.split(b';')
-        charset_value = charset.split(b'=')
-        return charset_value[1].strip().decode('ascii')
-
-    @staticmethod
-    def __convert_file_to_utf8(file, from_encoding):
-        tmp_file = file + '-tmp'
-        execute_command('iconv --from-code={0} --to-code=UTF-8 "{1}" > "{2}"'.format(from_encoding, file, tmp_file))
-        execute_command('rm "{0}"'.format(file))
-        execute_command('mv "{0}" "{1}"'.format(tmp_file, file))
 
 
 def main():
@@ -105,22 +113,26 @@ def main():
     if not args.path:
         raise Exception('--path parameter not specified')
 
-    if not args.extension:
-        raise Exception('--extension parameter not specified')
+    workers = []
 
-    if not args.dir_exclude:
-        raise Exception('--dir_exclude parameter not specified')
+    if args.prepend_pch:
+        workers.append(PrependPchWorker(args.prepend_pch))
 
-    if not args.prepend_pch:
-        raise Exception('--prepend_pch parameter not specified but this is only option')
+    if args.conv_to_utf8:
+        workers.append(ToUtf8Worker())
+
+    if not workers:
+        print('No command set. Nothing to do')
+        return
 
     all_target_files = get_all_files(args.path,
-                                     args.extension.split(),
-                                     args.dir_exclude.split(),
-                                     args.file_exclude.split())
+                                     args.extension.split() if args.extension else [],
+                                     args.dir_exclude.split() if args.dir_exclude else [],
+                                     args.file_exclude.split() if args.file_exclude else [])
 
-    worker = PrependPchWorker(all_target_files, args.prepend_pch)
-    worker.do()
+    for file in all_target_files:
+        for worker in workers:
+            worker.do(file)
 
 
 if __name__ == '__main__':
